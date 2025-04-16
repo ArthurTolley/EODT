74bc19d91e98a468fe870a60a4a569f1b87e7073
Arthur Tolley <arthurellistolley@gmail.com>
2025-04-16 20:23:50 +0100

    test change for github actions

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -10,7 +10,3 @@ def health_check():
 @router.post("/echo")
 def echo(payload: dict):
     return {"you_sent": payload}
-
-@router.get("/version")
-def get_version():
-    return {"version": "0.1.0"}
