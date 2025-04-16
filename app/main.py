94b677239e5c0c8cc6a8d8f1099019439a4934aa
Arthur Tolley <arthurellistolley@gmail.com>
2025-04-16 21:48:05 +0100

    basic change for v0.0.2

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import router as v1_router
 
-app = FastAPI(title="EODT - Road Usability API", version="0.0.1")
+app = FastAPI(title="EODT - Road Usability API", version="0.0.2")
 
 app.include_router(v1_router, prefix="/api/v1")
