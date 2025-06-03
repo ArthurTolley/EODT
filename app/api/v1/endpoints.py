d3b3e28a7e4b1fd0c09569d4e2507d8666926dba
Arthur Tolley <arthurellistolley@gmail.com>
2025-06-03 17:12:19 +0100

    Test Change for Image Update

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -1,6 +1,6 @@
 # app/api/v1/endpoints.py
 from fastapi import APIRouter
-from datetime import datetime
+from datetime import datetime, timezone
 from .schemas import EchoPayload, EchoResponse
 
 router = APIRouter()
@@ -14,5 +14,5 @@ def echo(payload: EchoPayload):
     return {
         "message": payload.message,
         "count": payload.count,
-        "timestamp": datetime.utcnow()
+        "timestamp": datetime.now(timezone.utc)  # Updated to use timezone-aware datetime
     }
--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import router as v1_router
 
-app = FastAPI(title="EODT - Road Usability API", version="0.0.2")
+# Test Change
+app = FastAPI(title="EODT - Road Usability API",
+              version="0.0.4")
 
 app.include_router(v1_router, prefix="/api/v1")
