3d23aa42ae653012ee05c9216b9b82861a85c616
Arthur Tolley <arthurellistolley@gmail.com>
2025-06-04 10:41:41 +0100

    test update

--- app/main.py
+++ app/main.py
@@ -4,6 +4,6 @@ from app.api.v1.endpoints import router as v1_router
 
 # Test Change
 app = FastAPI(title="EODT - Road Usability API",
-              version="0.0.4")
+              version="0.0.5")
 
 app.include_router(v1_router, prefix="/api/v1")
